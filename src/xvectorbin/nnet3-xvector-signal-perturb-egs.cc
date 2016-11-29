6bd441bee2a0c1e2f9ee7426db06c069b7531123
Hang Lyu <hlyu@a11.clsp.jhu.edu>
2016-11-29 02:23:14 -0500

    add a function--ApplyAddAdditiveNoise

--- src/xvectorbin/nnet3-xvector-signal-perturb-egs.cc
+++ src/xvectorbin/nnet3-xvector-signal-perturb-egs.cc
@@ -51,8 +51,8 @@ void ApplyPerturbation(XvectorPerturbOptions opts,
     // randomly generate an zero-phase FIR filter with no zeros.
     // In future, we can select trucated part of room impluse response
     // and convolve it with input_egs.
-    perturb_xvector.ComputeAndApplyRandDistortion(shifted_egs,
-                                  &rand_distort_shifted_egs);
+    ////perturb_xvector.ComputeAndApplyRandDistortion(shifted_egs,
+    ////                              &rand_distort_shifted_egs);
   }
 
   if (noise_egs) { 
@@ -73,16 +73,71 @@ void ApplyPerturbation(XvectorPerturbOptions opts,
   }
   // Perturb speed of signal egs
   Matrix<BaseFloat> warped_distorted_shifted_egs(rand_distort_shifted_egs);
-  if (opts.max_time_stretch != 0.0) 
-    perturb_xvector.TimeStretch(rand_distort_shifted_egs, 
-                                &warped_distorted_shifted_egs);
+  ////if (opts.max_time_stretch != 0.0) 
+  ////  perturb_xvector.TimeStretch(rand_distort_shifted_egs, 
+  ////                              &warped_distorted_shifted_egs);
    
   // If nagation is true, the sample values are randomly negated
   // with some probability.
-  if (opts.negation) {
+  ////if (opts.negation) {
    
-  }
+  ////}
 }
+
+// add
+// This function add the noise to the orginial signal. We should not normalize 
+// the signal level of the orginial signal. According to SNR, we rescale the noise
+// and add it. So that the perturbed signal is created. 
+void ApplyAddAdditiveNoise(const int32 &SNR,
+                           const Matrix<BaseFloat> &input_eg,
+                           const Matrix<BaseFloat> &noise_eg,
+                           Matrix<BaseFloat> *perturb_eg) {
+  // In the version, we ask the noise_cols >= input_cols. If mfcc, the cols are equal.
+  // If raw data, we ask the noise_cols > input_cols.
+  int32 input_rows = input_eg.NumRows(), input_cols = input_eg.NumCols();  
+  KALDI_ASSERT(noise_eg.NumCols() >= input_cols);
+
+  // According to the rows of noise_eg, form the noise_mat
+  // repeat the noise_eg blocks to have a new block which is longer than input_eg
+  Matrix<BaseFloat> noise_mat;
+  if (noise_eg.NumRows() < input_rows) {
+    int32 repeat_times = (input_rows / noise_eg.NumRows()) + 1;
+    noise_mat.Resize(noise_eg.NumRows() * repeat_times, noise_eg.NumCols());
+    for (int32 i = 0; i < repeat_times; ++i) {
+      noise_mat.Range(i*noise_eg.NumRows(), noise_eg.NumRows(), 
+                      0, noise_eg.NumCols()).CopyFromMat(noise_eg);
+    }
+  } else {
+    noise_mat.Resize(noise_eg.NumRows(), noise_eg.NumCols());
+    noise_mat.CopyFromMat(noise_eg);
+  }
+
+  // select the noise range
+  int32 noise_rows = noise_mat.NumRows(), noise_cols = noise_mat.NumCols();
+  int32 start_row_ind = RandInt(0, noise_rows - input_rows),
+        start_col_ind = RandInt(0, noise_cols - input_cols); 
+  Matrix<BaseFloat> selected_noise_mat(input_rows, input_cols);
+  selected_noise_mat.AddMat(1.0, noise_mat.Range(start_row_ind, input_rows,
+                                                  start_col_ind, input_cols));
+  // compute the energy of noise and input
+  Matrix<BaseFloat> input_energy_mat(input_rows, input_cols);
+  input_energy_mat.AddMatMatElements(1.0, input_eg, input_eg, 1.0);
+  double input_energy = input_energy_mat.Sum();
+  Matrix<BaseFloat> noise_energy_mat(input_rows, input_cols);
+  noise_energy_mat.AddMatMatElements(1.0, selected_noise_mat, selected_noise_mat, 1.0);
+  double noise_energy = noise_energy_mat.Sum();
+
+  // In Energy domain, SNR=20log10(S/N). 
+  // 10^(SNR/20) = input_energy / (scale^2 * noise_energy)
+  double scale = input_energy / noise_energy / (pow(10,SNR/20));
+  scale = sqrt(scale);
+  
+  // Add noise mat to input_eg mat
+  perturb_eg->Resize(input_rows, input_cols);
+  perturb_eg->CopyFromMat(input_eg);
+  perturb_eg->AddMat(scale, selected_noise_mat);
+}
+// add-end
 
 } // end of namespace nnet3
 } // end of namespace kaldi
@@ -108,6 +163,14 @@ int main(int argc, char *argv[]) {
     XvectorPerturbOptions perturb_opts;
     perturb_opts.Register(&po);
 
+    // add
+    std::string add_noise_rspecifier;
+    po.Register("add-noise", &add_noise_rspecifier, "specify a file contains some noise egs");
+    int32 snr;
+    po.Register("SNR",&snr,"specify a Signal to Noise Ration.We will scale the noise according \
+                to the original signal and SNR. Normally, it's a non-zero number between -30 and 30");
+    // add-end
+
     po.Read(argc, argv);
     if (po.NumArgs() != 2) {
       po.PrintUsage();
@@ -120,6 +183,20 @@ int main(int argc, char *argv[]) {
     SequentialNnetExampleReader example_reader(examples_rspecifier);
      
     NnetExampleWriter example_writer(examples_wspecifier);
+
+    // add
+    // count the number of noise examples and record the key
+    std::vector<std::string> list_noise_egs;
+    SequentialNnetExampleReader noise_seq_reader(add_noise_rspecifier);
+    for (; !noise_seq_reader.Done(); noise_seq_reader.Next()) {
+      std::string key = noise_seq_reader.Key();
+      list_noise_egs.push_back(key);
+    }
+    noise_seq_reader.Close();
+    int32 num_noise_egs = list_noise_egs.size();
+    // initial a RandomAccessTableReader for noise egs
+    RandomAccessNnetExampleReader noise_random_reader(add_noise_rspecifier);
+    // add-end
 
     int64 num_read = 0, num_written = 0;
 
@@ -141,7 +218,24 @@ int main(int argc, char *argv[]) {
       Matrix<BaseFloat> perturb_eg_mat, 
         input_eg_mat;
       input_eg_io.features.CopyToMat(&input_eg_mat);
-      ApplyPerturbation(perturb_opts, input_eg_mat, noise_mat, &perturb_eg_mat);
+      
+      // add
+      if (!add_noise_rspecifier.empty()) {
+        // random choose a noise example
+        int32 index_noise_egs = RandInt(0, num_noise_egs - 1);
+        std::string key_noise_egs = list_noise_egs[index_noise_egs];
+        const NnetExample &noise_eg = noise_random_reader.Value(key_noise_egs);
+        const NnetIo &noise_eg_io = noise_eg.io[0];
+        
+        Matrix<BaseFloat> noise_eg_mat;
+        noise_eg_io.features.CopyToMat(&noise_eg_mat);
+
+        // deal with add noise
+        ApplyAddAdditiveNoise(snr, input_eg_mat, noise_eg_mat, &perturb_eg_mat);
+      } else {
+        ApplyPerturbation(perturb_opts, input_eg_mat, noise_mat, &perturb_eg_mat);
+      }
+      // add-end
       perturb_eg->io.resize(1.0);
       perturb_eg->io[0].features.SwapFullMatrix(&perturb_eg_mat);
       example_writer.Write(key, *perturb_eg);
