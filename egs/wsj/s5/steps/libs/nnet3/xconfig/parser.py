2d256ad3a5aba1282502bc2dfe1f1a1657fea7c4
Daniel Povey <dpovey@gmail.com>
2018-03-18 14:08:27 -0400

    [egs] Add improved TDNN-F example for Switchboard. (#2291)
    Conflicts:
    	egs/wsj/s5/steps/libs/nnet3/xconfig/parser.py
    	egs/wsj/s5/steps/libs/nnet3/xconfig/trivial_layers.py

--- egs/wsj/s5/steps/libs/nnet3/xconfig/parser.py
+++ egs/wsj/s5/steps/libs/nnet3/xconfig/parser.py
@@ -79,7 +79,10 @@ config_to_layer = {
         'opgru-layer' : xlayers.XconfigOpgruLayer,
         'norm-pgru-layer' : xlayers.XconfigNormPgruLayer,
         'norm-opgru-layer' : xlayers.XconfigNormOpgruLayer,
-        'renorm-component': xlayers.XconfigRenormComponent
+        'renorm-component': xlayers.XconfigRenormComponent,
+        'batchnorm-component': xlayers.XconfigBatchnormComponent,
+        'no-op-component': xlayers.XconfigNoOpComponent,
+        'linear-component': xlayers.XconfigLinearComponent
 }
 
 # Turn a config line and a list of previous layers into
